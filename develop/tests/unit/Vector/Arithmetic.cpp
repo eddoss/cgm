2e7bf28ab9c5acfae0b8f8f9d54d88eb164866f5
Edward Sarkisyan <edw.sarkisyan@gmail.com>
2020-01-31 19:58:51 +0200

    Fixed vector tests.

--- develop/tests/unit/Vector/Arithmetic.cpp
+++ develop/tests/unit/Vector/Arithmetic.cpp
@@ -7,8 +7,10 @@
 
 
 using namespace std;
-using Vec = Vector<3>;
-
+using Vec2 = Vector<2>;
+using Vec3 = Vector<3>;
+using Vec4 = Vector<4>;
+using Vec5 = Vector<5>;
 
 /* ####################################################################################### */
 /* Arithmetic operators: unary minus */
@@ -16,11 +18,36 @@ using Vec = Vector<3>;
 
 TEST(Vector_Arithmetic, UnaryMinus)
 {
-    Vec input {1,2,3};
-    Vec expec {-1,-2,-3};
-    input = -input;
+    {
+        Vec2 vec { 1, 2 };
+        Vec2 exp {-1,-2 };
+        ASSERT_EQ(-vec, exp);
+    }
 
-    for (Vec::size_type i = 0; i < Vec::dimensions; ++i) ASSERT_EQ(input[i], expec[i]);
+    /* -------------- */
+
+    {
+        Vec3 vec { 1, 2, 3 };
+        Vec3 exp {-1,-2,-3 };
+        ASSERT_EQ(-vec, exp);
+    }
+
+    /* -------------- */
+
+    {
+        Vec4 vec { 1, 2, 3, 4 };
+        Vec4 exp {-1,-2,-3,-4 };
+        ASSERT_EQ(-vec, exp);
+    }
+
+
+    /* -------------- */
+
+    {
+        Vec5 vec { 1, 2, 3, 4, 5 };
+        Vec5 exp {-1,-2,-3,-4,-5 };
+        ASSERT_EQ(-vec, exp);
+    }
 }
 
 /* ####################################################################################### */
@@ -29,23 +56,82 @@ TEST(Vector_Arithmetic, UnaryMinus)
 
 TEST(Vector_Arithmetic, InplaceMinus_Scalar)
 {
-    Vec input {1,2,3};
-    Vec expec {-1,0,1};
-    input -= Vec::value_type(2);
+    {
+        Vec2 vec { 1, 2 };
+        Vec2 exp { 0, 1 };
+        vec -= Vec2::value_type(1);
+        ASSERT_EQ(vec, exp);
+    }
 
-    for (Vec::size_type i = 0; i < Vec::dimensions; ++i) ASSERT_EQ(input[i], expec[i]);
+    /* -------------- */
+
+    {
+        Vec3 vec { 1, 2, 3 };
+        Vec3 exp { 0, 1, 2 };
+        vec -= Vec3::value_type(1);
+        ASSERT_EQ(vec, exp);
+    }
+
+    /* -------------- */
+
+    {
+        Vec4 vec { 1, 2, 3, 4 };
+        Vec4 exp { 0, 1, 2, 3 };
+        vec -= Vec4::value_type(1);
+        ASSERT_EQ(vec, exp);
+    }
+
+    /* -------------- */
+
+    {
+        Vec5 vec { 1, 2, 3, 4, 5 };
+        Vec5 exp { 0, 1, 2, 3, 4 };
+        vec -= Vec5::value_type(1);
+        ASSERT_EQ(vec, exp);
+    }
 }
 
 /* --------------------------------------------------------------------------------------- */
 
 TEST(Vector_Arithmetic, InplaceMinus_Other)
 {
-    Vec input {1,2,3};
-    Vec other {2,3,4};
-    Vec expec {-1,-1,-1};
-    input -= other;
+    {
+        Vec2 vec { 1, 2 };
+        Vec2 oth { 1, 1 };
+        Vec2 exp { 0, 1 };
+        vec -= oth;
+        ASSERT_EQ(vec, exp);
+    }
 
-    for (Vec::size_type i = 0; i < Vec::dimensions; ++i) ASSERT_EQ(input[i], expec[i]);
+    /* -------------- */
+
+    {
+        Vec3 vec { 1, 2, 3 };
+        Vec3 oth { 1, 1, 2 };
+        Vec3 exp { 0, 1, 1 };
+        vec -= oth;
+        ASSERT_EQ(vec, exp);
+    }
+
+    /* -------------- */
+
+    {
+        Vec4 vec { 1, 2, 3, 4 };
+        Vec4 oth { 1, 1, 2, 3 };
+        Vec4 exp { 0, 1, 1, 1 };
+        vec -= oth;
+        ASSERT_EQ(vec, exp);
+    }
+
+    /* -------------- */
+
+    {
+        Vec5 vec { 1, 2, 3, 4, 5 };
+        Vec5 oth { 1, 1, 2, 3, 4 };
+        Vec5 exp { 0, 1, 1, 1, 1 };
+        vec -= oth;
+        ASSERT_EQ(vec, exp);
+    }
 }
 
 /* ####################################################################################### */
@@ -54,23 +140,82 @@ TEST(Vector_Arithmetic, InplaceMinus_Other)
 
 TEST(Vector_Arithmetic, InplacePlus_Scalar)
 {
-    Vec input {1,2,3};
-    Vec expec {3,4,5};
-    input += Vec::value_type(2);
+    {
+        Vec2 vec { 1, 2 };
+        Vec2 exp { 2, 3 };
+        vec += Vec2::value_type(1);
+        ASSERT_EQ(vec, exp);
+    }
 
-    for (Vec::size_type i = 0; i < Vec::dimensions; ++i) ASSERT_EQ(input[i], expec[i]);
+    /* -------------- */
+
+    {
+        Vec3 vec { 1, 2, 3 };
+        Vec3 exp { 2, 3, 4 };
+        vec += Vec3::value_type(1);
+        ASSERT_EQ(vec, exp);
+    }
+
+    /* -------------- */
+
+    {
+        Vec4 vec { 1, 2, 3, 4 };
+        Vec4 exp { 2, 3, 4, 5 };
+        vec += Vec4::value_type(1);
+        ASSERT_EQ(vec, exp);
+    }
+
+    /* -------------- */
+
+    {
+        Vec5 vec { 1, 2, 3, 4, 5 };
+        Vec5 exp { 2, 3, 4, 5, 6 };
+        vec += Vec5::value_type(1);
+        ASSERT_EQ(vec, exp);
+    }
 }
 
 /* --------------------------------------------------------------------------------------- */
 
 TEST(Vector_Arithmetic, InplacePlus_Other)
 {
-    Vec input {1,2,3};
-    Vec other {2,3,4};
-    Vec expec {3,5,7};
-    input += other;
+    {
+        Vec2 vec { 1, 2 };
+        Vec2 oth { 1, 1 };
+        Vec2 exp { 2, 3 };
+        vec += oth;
+        ASSERT_EQ(vec, exp);
+    }
 
-    for (Vec::size_type i = 0; i < Vec::dimensions; ++i) ASSERT_EQ(input[i], expec[i]);
+    /* -------------- */
+
+    {
+        Vec3 vec { 1, 2, 3 };
+        Vec3 oth { 1, 1, 2 };
+        Vec3 exp { 2, 3, 5 };
+        vec += oth;
+        ASSERT_EQ(vec, exp);
+    }
+
+    /* -------------- */
+
+    {
+        Vec4 vec { 1, 2, 3, 4 };
+        Vec4 oth { 1, 1, 2, 3 };
+        Vec4 exp { 2, 3, 5, 7 };
+        vec += oth;
+        ASSERT_EQ(vec, exp);
+    }
+
+    /* -------------- */
+
+    {
+        Vec5 vec { 1, 2, 3, 4, 5 };
+        Vec5 oth { 1, 1, 2, 3, 4 };
+        Vec5 exp { 2, 3, 5, 7, 9 };
+        vec += oth;
+        ASSERT_EQ(vec, exp);
+    }
 }
 
 /* ####################################################################################### */
@@ -79,23 +224,82 @@ TEST(Vector_Arithmetic, InplacePlus_Other)
 
 TEST(Vector_Arithmetic, InplacePerComponentMultiplication_Scalar)
 {
-    Vec input {1,2,3};
-    Vec expec {2,4,6};
-    input *= Vec::value_type(2);
+    {
+        Vec2 vec { 1, 2 };
+        Vec2 exp { 3, 6 };
+        vec *= Vec2::value_type(3);
+        ASSERT_EQ(vec, exp);
+    }
 
-    for (Vec::size_type i = 0; i < Vec::dimensions; ++i) ASSERT_EQ(input[i], expec[i]);
+    /* -------------- */
+
+    {
+        Vec3 vec { 1, 2, 3 };
+        Vec3 exp { 3, 6, 9 };
+        vec *= Vec3::value_type(3);
+        ASSERT_EQ(vec, exp);
+    }
+
+    /* -------------- */
+
+    {
+        Vec4 vec { 1, 2, 3, 4 };
+        Vec4 exp { 2, 4, 6, 8 };
+        vec *= Vec4::value_type(2);
+        ASSERT_EQ(vec, exp);
+    }
+
+    /* -------------- */
+
+    {
+        Vec5 vec { 1, 2, 3, 4, 5 };
+        Vec5 exp { 2, 4, 6, 8, 10 };
+        vec *= Vec5::value_type(2);
+        ASSERT_EQ(vec, exp);
+    }
 }
 
 /* --------------------------------------------------------------------------------------- */
 
 TEST(Vector_Arithmetic, InplacePerComponentMultiplication_Other)
 {
-    Vec input {1,2,3};
-    Vec other {2,3,4};
-    Vec expec {2,6,12};
-    input *= other;
+    {
+        Vec2 vec { 1, 2 };
+        Vec2 oth { 2, 2 };
+        Vec2 exp { 2, 4 };
+        vec *= oth;
+        ASSERT_EQ(vec, exp);
+    }
 
-    for (Vec::size_type i = 0; i < Vec::dimensions; ++i) ASSERT_EQ(input[i], expec[i]);
+    /* -------------- */
+
+    {
+        Vec3 vec { 1, 2, 3 };
+        Vec3 oth { 2, 3, 2 };
+        Vec3 exp { 2, 6, 6 };
+        vec *= oth;
+        ASSERT_EQ(vec, exp);
+    }
+
+    /* -------------- */
+
+    {
+        Vec4 vec { 1, 2, 3, 4 };
+        Vec4 oth { 2, 3, 3, 2 };
+        Vec4 exp { 2, 6, 9, 8 };
+        vec *= oth;
+        ASSERT_EQ(vec, exp);
+    }
+
+    /* -------------- */
+
+    {
+        Vec5 vec { 1, 2, 3, 4, 5 };
+        Vec5 oth { 2, 2, 3, 2, 1 };
+        Vec5 exp { 2, 4, 9, 8, 5 };
+        vec *= oth;
+        ASSERT_EQ(vec, exp);
+    }
 }
 
 /* ####################################################################################### */
@@ -104,21 +308,80 @@ TEST(Vector_Arithmetic, InplacePerComponentMultiplication_Other)
 
 TEST(Vector_Arithmetic, InplaceDivision_Scalar)
 {
-    Vec input {4,6,10};
-    Vec expec {2,3,5};
-    input /= Vec::value_type(2);
+    {
+        Vec2 vec { 2, 4 };
+        Vec2 exp { 1, 2 };
+        vec /= Vec2::value_type(2);
+        ASSERT_EQ(vec, exp);
+    }
 
-    for (Vec::size_type i = 0; i < Vec::dimensions; ++i) ASSERT_EQ(input[i], expec[i]);
+    /* -------------- */
+
+    {
+        Vec3 vec { 3, 6, 9 };
+        Vec3 exp { 1, 2, 3 };
+        vec /= Vec3::value_type(3);
+        ASSERT_EQ(vec, exp);
+    }
+
+    /* -------------- */
+
+    {
+        Vec4 vec { 2, 4, 6, 8 };
+        Vec4 exp { 1, 2, 3, 4 };
+        vec /= Vec4::value_type(2);
+        ASSERT_EQ(vec, exp);
+    }
+
+    /* -------------- */
+
+    {
+        Vec5 vec { 2, 4, 6, 8, 10 };
+        Vec5 exp { 1, 2, 3, 4, 5 };
+        vec /= Vec5::value_type(2);
+        ASSERT_EQ(vec, exp);
+    }
 }
 
 /* --------------------------------------------------------------------------------------- */
 
 TEST(Vector_Arithmetic, InplaceDivision_Other)
 {
-    Vec input {10,20,30};
-    Vec other {2,10,6};
-    Vec expec {5,2,5};
-    input /= other;
+    {
+        Vec2 vec { 1, 2 };
+        Vec2 oth { 1, 2 };
+        Vec2 exp { 1, 1 };
+        vec /= oth;
+        ASSERT_EQ(vec, exp);
+    }
 
-    for (Vec::size_type i = 0; i < Vec::dimensions; ++i) ASSERT_EQ(input[i], expec[i]);
+    /* -------------- */
+
+    {
+        Vec3 vec { 4, 6, 8 };
+        Vec3 oth { 2, 2, 2 };
+        Vec3 exp { 2, 3, 4 };
+        vec /= oth;
+        ASSERT_EQ(vec, exp);
+    }
+
+    /* -------------- */
+
+    {
+        Vec4 vec { 2, 4, 6, 8 };
+        Vec4 oth { 2, 2, 2, 2 };
+        Vec4 exp { 1, 2, 3, 4 };
+        vec /= oth;
+        ASSERT_EQ(vec, exp);
+    }
+
+    /* -------------- */
+
+    {
+        Vec5 vec { 2, 4, 6, 8, 10 };
+        Vec5 oth { 2, 2, 2, 2, 2 };
+        Vec5 exp { 1, 2, 3, 4, 5 };
+        vec /= oth;
+        ASSERT_EQ(vec, exp);
+    }
 }
