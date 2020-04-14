e3556b4ca3f7908863c00df3f89f419d865606de
Edward Sarkisyan <edw.sarkisyan@gmail.com>
2020-04-14 18:57:42 +0200

    Fixed XForm::Transform fields order layout.

--- develop/include/Math3D/XForm/Transforms.hpp
+++ develop/include/Math3D/XForm/Transforms.hpp
@@ -44,6 +44,12 @@ template<size_t D, typename T>
 struct Transforms
 {
     /**
+     * Transformations pivot point.
+     */
+    Pivot<D,T>
+    pivot;
+
+    /**
      * Describe translations.
      */
     Vector<D,T>
@@ -66,12 +72,6 @@ struct Transforms
      */
     T
     uniformScale;
-
-    /**
-     * Transformations pivot point.
-     */
-    Pivot<D,T>
-    pivot;
 
     /**
      * Describe transformation order.
