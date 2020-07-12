b7523491d80e49f24404cf7f3ad0c2dd16270a5f
Edward Sarkisyan <edw.sarkisyan@gmail.com>
2020-07-13 00:37:32 +0200

    [core]: replaced common constants numbers (BIG_NUMBER, PI, PI2 ...) from Global.hpp to Common.hpp. [core]: fixed platforms macro.

--- develop/include/Math3D/Global.hpp
+++ develop/include/Math3D/Global.hpp
@@ -6,7 +6,7 @@
 
 
 /* ####################################################################################### */
-/* Library namespaces */
+/* Library namespace */
 /* ####################################################################################### */
 
 #define MATH3D_NAMESPACE        mth
@@ -31,58 +31,16 @@
 #define MATH3D_COORD_NAMESPACE_END      }
 
 /* ####################################################################################### */
-/* Common constants */
-/* ####################################################################################### */
-
-#ifdef MATH3D_USE_DOUBLE_PRECISION
-    using FLOAT = double;
-    constexpr FLOAT BIG_NUMBER                  = 1.79768e+308;
-    constexpr FLOAT SMALL_NEGATIVE_NUMBER       = 2.22506e-308;
-    constexpr FLOAT SMALL_POSITIVE_NUMBER       = 0.0000000000001;
-    constexpr FLOAT PI                          = 3.1415926535897;
-    constexpr FLOAT PI2                         = 6.2831853071795;
-    constexpr FLOAT PI_HALF                     = 1.5707963267948;
-    constexpr FLOAT PI_INVERTED                 = 0.3183098861837;
-    constexpr FLOAT EULER_NUMBER                = 2.7182818284590;
-    constexpr FLOAT GOLDEN_RATIO                = 1.6180339887498;
-    constexpr FLOAT TOLERANCE                   = 0.0000000000001;
-    constexpr FLOAT TO_DEGREES                  = 57.295779513000;
-    constexpr FLOAT TO_RADIANS                  = 0.0174532925199;
-#else
-    using FLOAT = float;
-    constexpr FLOAT BIG_NUMBER                  = 3.40282e+38f;
-    constexpr FLOAT SMALL_POSITIVE_NUMBER       = 0.00000001f;
-    constexpr FLOAT SMALL_NEGATIVE_NUMBER       = 1.17548e-38f;
-    constexpr FLOAT PI                          = 3.14159265f;
-    constexpr FLOAT PI2                         = 6.28318530f;
-    constexpr FLOAT PI_HALF                     = 1.57079632f;
-    constexpr FLOAT PI_INVERTED                 = 0.31830988f;
-    constexpr FLOAT EULER_NUMBER                = 2.71828182f;
-    constexpr FLOAT GOLDEN_RATIO                = 1.61803398f;
-    constexpr FLOAT TOLERANCE                   = 0.00000001f;
-    constexpr FLOAT TO_DEGREES                  = 57.2957790f;
-    constexpr FLOAT TO_RADIANS                  = 0.01745320f;
-#endif
-
-/* ####################################################################################### */
 /* Platform */
 /* ####################################################################################### */
 
 #ifdef __GNUC__
     #define FORCEINLINE     inline __attribute__((always_inline))
     #define FORCENOINLINE   __attribute__((noinline))
-#endif
-
-/* --------------------------------------------------------------------------------------- */
-
-#ifdef __clang__
+#elif __clang__
     #define FORCEINLINE     inline __attribute__((always_inline))
     #define FORCENOINLINE   __attribute__((noinline))
-#endif
-
-/* --------------------------------------------------------------------------------------- */
-
-#ifdef _MSC_VER
+#elif _MSC_VER
     #define FORCEINLINE     __forceinline
     #define FORCENOINLINE   __declspec(noinline)
 #endif
