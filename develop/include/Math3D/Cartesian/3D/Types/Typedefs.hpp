192475b1a6cb4d5577ed3d06ac94e9fa03e91807
Edward Sarkisyan <edw.sarkisyan@gmail.com>
2020-07-22 01:52:34 +0200

    [cartesian::3D]: fix default type for AxesTuple and SpaceTuple.

--- develop/include/Math3D/Cartesian/3D/Types/Typedefs.hpp
+++ develop/include/Math3D/Cartesian/3D/Types/Typedefs.hpp
@@ -9,8 +9,8 @@
 MATH3D_NAMESPACE_BEGIN
 MATH3D_XYZ_NAMESPACE_BEGIN
 
-template<typename T> using AxesTuple    = std::tuple<Vector<3,T>, Vector<3,T>, Vector<3,T>>;
-template<typename T> using SpaceTuple   = std::tuple<Vector<3,T>, Vector<3,T>, Vector<3,T>, Vector<3,T>>;
+template<typename T=FLOAT> using AxesTuple    = std::tuple<Vector<3,T>, Vector<3,T>, Vector<3,T>>;
+template<typename T=FLOAT> using SpaceTuple   = std::tuple<Vector<3,T>, Vector<3,T>, Vector<3,T>, Vector<3,T>>;
 
 MATH3D_XYZ_NAMESPACE_END
 MATH3D_NAMESPACE_END
