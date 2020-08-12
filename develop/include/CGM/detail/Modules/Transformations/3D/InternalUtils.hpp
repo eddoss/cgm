064b5879351ccc53e9603dfc5126662f9ed1552a
Edward Sarkisyan <edw.sarkisyan@gmail.com>
2020-08-12 22:53:46 +0200

    [transformations::3D]: added internal utils.

---
+++ develop/include/CGM/detail/Modules/Transformations/3D/InternalUtils.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+
+#include <CGM/Modules/Transformations/3D/ModuleGlobals.hpp>
+#include <CGM/Modules/Core/Types/Matrix.hpp>
+#include <CGM/Modules/Core/Operators/Matrix.hpp>
+
+
+CGM_NAMESPACE_BEGIN
+CGM_XFORM3D_NAMESPACE_BEGIN namespace detail {
+
+template<typename T>
+constexpr CGM_FORCEINLINE void
+multiply_matrix3x3_on_matrix4x4(Matrix<3,3,T>& mat3, const Matrix<4,4,T>& mat4)
+{
+    mat3 *= Matrix<3,3,T>
+    {
+        mat4(0,0), mat4(0,1), mat4(0,2),
+        mat4(1,0), mat4(1,1), mat4(1,2),
+        mat4(2,0), mat4(2,1), mat4(2,2)
+    };
+}
+
+/* --------------------------------------------------------------------------------------- */
+
+template<typename T>
+constexpr CGM_FORCEINLINE void
+multiply_matrix4x4_on_matrix3x3(Matrix<4,4,T>& mat4, const Matrix<3,3,T>& mat3)
+{
+    mat4 *= Matrix<4,4,T>
+    {
+        mat3(0,0), mat3(0,1), mat3(0,2), zero<T>,
+        mat3(1,0), mat3(1,1), mat3(1,2), zero<T>,
+        mat3(2,0), mat3(2,1), mat3(2,2), zero<T>,
+        zero<T>, zero<T>, zero<T>, number<1>(1)
+    };
+}
+
+}CGM_XFORM3D_NAMESPACE_END
+CGM_NAMESPACE_END
