4adec1e0c70e532872960cf283fef414e60c1844
Edward Sarkisyan <edw.sarkisyan@gmail.com>
2020-08-20 12:40:53 +0200

    [cartesian::3D]: fixed 'LocalToLocal' converters tests.

--- develop/tests/Cartesian/3D/Functions/Converters/LocalToLocal/Matrix3WithPosition.cpp
+++ develop/tests/Cartesian/3D/Functions/Converters/LocalToLocal/Matrix3WithPosition.cpp
@@ -2,51 +2,26 @@
 
 #include <iostream>
 #include <gtest/gtest.h>
-#include <CGM/Modules/Core/Operators/Matrix.hpp>
-#include <CGM/Modules/Core/Functions/Matrix.hpp>
 #include <CGM/Modules/Core/Functions/Vector.hpp>
-#include <CGM/Modules/Core/Functions/Quaternion.hpp>
-#include <CGM/Modules/Cartesian/3D/Types/Enums.hpp>
-#include <CGM/Modules/Cartesian/3D/Functions/Utils.hpp>
 #include <CGM/Modules/Cartesian/3D/Functions/Converters.hpp>
-#include <CGM/Modules/Cartesian/3D/Functions/Orientation.hpp>
+#include "Input.hpp"
 
 
 using namespace std;
-using namespace CGM;
 
-constexpr EVectorRepresentation L2LMAT3P_POINT = EVectorRepresentation::Point;
-constexpr EVectorRepresentation L2LMAT3P_DIRECTION = EVectorRepresentation::Direction;
-
-static const auto L2LMAT3P_A_COORD = Vector<3,double>{ 3.13, 2.2, 1.7 };
-
-static const auto L2LMAT3P_A_X = Vector<3,double>{ +0.559270, +0.302442, -0.77184 };
-static const auto L2LMAT3P_A_Y = Vector<3,double>{ -0.313298, +0.939132, +0.14098 };
-static const auto L2LMAT3P_A_Z = Vector<3,double>{ +0.767503, +0.162971, +0.61998 };
-static const auto L2LMAT3P_A_P = Vector<3,double>{ -1.200000, -0.600000, +4.40000 };
-static const auto L2LMAT3P_A_MAT3 = CGM_XYZ::orientationMatrix(L2LMAT3P_A_X, L2LMAT3P_A_Y, L2LMAT3P_A_Z);
-
-static const auto L2LMAT3P_B_X = Vector<3,double>{ +0.915829, -0.386754, -0.108065 };
-static const auto L2LMAT3P_B_Y = Vector<3,double>{ +0.367711, +0.915829, -0.161386 };
-static const auto L2LMAT3P_B_Z = Vector<3,double>{ +0.161386, +0.108065, +0.980957 };
-static const auto L2LMAT3P_B_P = Vector<3,double>{ +4.030000, +1.700000, +2.200000 };
-static const auto L2LMAT3P_B_QUAT = Quaternion<double>{ -0.068998, 0.068998, -0.193196, 0.976296 };
-static const auto L2LMAT3P_B_MAT4 = CGM_XYZ::spaceMatrix(L2LMAT3P_B_X, L2LMAT3P_B_Y, L2LMAT3P_B_Z, L2LMAT3P_B_P);
-static const auto L2LMAT3P_B_MAT3 = CGM_XYZ::orientationMatrix(L2LMAT3P_B_X, L2LMAT3P_B_Y, L2LMAT3P_B_Z);
-static const auto L2LMAT3P_B_COORD_PT_PT = Vector<3,double>{ -3.166704, +1.353024, +6.503487 };
-static const auto L2LMAT3P_B_COORD_PT_DIR = Vector<3,double>{ +1.504246, +1.589060, +7.951732 };
-static const auto L2LMAT3P_B_COORD_DIR_DIR = Vector<3,double>{ +2.113769, +1.198966, +3.409012 };
 
 TEST(Cartesian_3D_Functions_Converters, LocalToLocal_Mat3WithPos_Mat3)
 {
+    using namespace cgm_xyz_converters_tests_data;
+
     {
-        auto result = CGM_XYZ::localToLocal<L2LMAT3P_POINT>(L2LMAT3P_A_COORD, L2LMAT3P_A_MAT3, L2LMAT3P_A_P, L2LMAT3P_B_MAT3);
-        ASSERT_TRUE(CGM::eq(result, L2LMAT3P_B_COORD_PT_DIR, 0.0001));
+        auto result = CGM_XYZ::localToLocal<CGM_POINT>(A_LOCAL_COORD, A_MAT3, A_P, B_MAT3);
+        ASSERT_TRUE(CGM::eq(result, A_LOCAL_COORD_CONVERTED_TO_LOCAL_B__SPACE_2_ORIENT, 0.0001));
     }
 
     {
-        auto result = CGM_XYZ::localToLocal<L2LMAT3P_DIRECTION>(L2LMAT3P_A_COORD, L2LMAT3P_A_MAT3, L2LMAT3P_A_P, L2LMAT3P_B_MAT3);
-        ASSERT_TRUE(CGM::eq(result, L2LMAT3P_B_COORD_DIR_DIR, 0.0001));
+        auto result = CGM_XYZ::localToLocal<CGM_DIRECTION>(A_LOCAL_COORD, A_MAT3, A_P, B_MAT3);
+        ASSERT_TRUE(CGM::eq(result, A_LOCAL_COORD_CONVERTED_TO_LOCAL_B__ORIENT_2_ORIENT, 0.0001));
     }
 }
 
@@ -54,14 +29,16 @@ TEST(Cartesian_3D_Functions_Converters, LocalToLocal_Mat3WithPos_Mat3)
 
 TEST(Cartesian_3D_Functions_Converters, LocalToLocal_Mat3WithPos_Mat3WithPos)
 {
+    using namespace cgm_xyz_converters_tests_data;
+
     {
-        auto result = CGM_XYZ::localToLocal<L2LMAT3P_POINT>(L2LMAT3P_A_COORD, L2LMAT3P_A_MAT3, L2LMAT3P_A_P, L2LMAT3P_B_MAT3, L2LMAT3P_B_P);
-        ASSERT_TRUE(CGM::eq(result, L2LMAT3P_B_COORD_PT_PT, 0.0001));
+        auto result = CGM_XYZ::localToLocal<CGM_POINT>(A_LOCAL_COORD, A_MAT3, A_P, B_MAT3, B_P);
+        ASSERT_TRUE(CGM::eq(result, A_LOCAL_COORD_CONVERTED_TO_LOCAL_B__SPACE_2_SPACE, 0.0001));
     }
 
     {
-        auto result = CGM_XYZ::localToLocal<L2LMAT3P_DIRECTION>(L2LMAT3P_A_COORD, L2LMAT3P_A_MAT3, L2LMAT3P_A_P, L2LMAT3P_B_MAT3, L2LMAT3P_B_P);
-        ASSERT_TRUE(CGM::eq(result, L2LMAT3P_B_COORD_DIR_DIR, 0.0001));
+        auto result = CGM_XYZ::localToLocal<CGM_DIRECTION>(A_LOCAL_COORD, A_MAT3, A_P, B_MAT3, B_P);
+        ASSERT_TRUE(CGM::eq(result, A_LOCAL_COORD_CONVERTED_TO_LOCAL_B__ORIENT_2_ORIENT, 0.0001));
     }
 }
 
@@ -69,14 +46,16 @@ TEST(Cartesian_3D_Functions_Converters, LocalToLocal_Mat3WithPos_Mat3WithPos)
 
 TEST(Cartesian_3D_Functions_Converters, LocalToLocal_Mat3WithPos_Mat4)
 {
+    using namespace cgm_xyz_converters_tests_data;
+
     {
-        auto result = CGM_XYZ::localToLocal<L2LMAT3P_POINT>(L2LMAT3P_A_COORD, L2LMAT3P_A_MAT3, L2LMAT3P_A_P, L2LMAT3P_B_MAT4);
-        ASSERT_TRUE(CGM::eq(result, L2LMAT3P_B_COORD_PT_PT, 0.0001));
+        auto result = CGM_XYZ::localToLocal<CGM_POINT>(A_LOCAL_COORD, A_MAT3, A_P, B_MAT4);
+        ASSERT_TRUE(CGM::eq(result, A_LOCAL_COORD_CONVERTED_TO_LOCAL_B__SPACE_2_SPACE, 0.0001));
     }
 
     {
-        auto result = CGM_XYZ::localToLocal<L2LMAT3P_DIRECTION>(L2LMAT3P_A_COORD, L2LMAT3P_A_MAT3, L2LMAT3P_A_P, L2LMAT3P_B_MAT4);
-        ASSERT_TRUE(CGM::eq(result, L2LMAT3P_B_COORD_DIR_DIR, 0.0001));
+        auto result = CGM_XYZ::localToLocal<CGM_DIRECTION>(A_LOCAL_COORD, A_MAT3, A_P, B_MAT4);
+        ASSERT_TRUE(CGM::eq(result, A_LOCAL_COORD_CONVERTED_TO_LOCAL_B__ORIENT_2_ORIENT, 0.0001));
     }
 }
 
@@ -84,14 +63,16 @@ TEST(Cartesian_3D_Functions_Converters, LocalToLocal_Mat3WithPos_Mat4)
 
 TEST(Cartesian_3D_Functions_Converters, LocalToLocal_Mat3WithPos_Quat)
 {
+    using namespace cgm_xyz_converters_tests_data;
+
     {
-        auto result = CGM_XYZ::localToLocal<L2LMAT3P_POINT>(L2LMAT3P_A_COORD, L2LMAT3P_A_MAT3, L2LMAT3P_A_P, L2LMAT3P_B_QUAT);
-        ASSERT_TRUE(CGM::eq(result, L2LMAT3P_B_COORD_PT_DIR, 0.0001));
+        auto result = CGM_XYZ::localToLocal<CGM_POINT>(A_LOCAL_COORD, A_MAT3, A_P, B_QUAT);
+        ASSERT_TRUE(CGM::eq(result, A_LOCAL_COORD_CONVERTED_TO_LOCAL_B__SPACE_2_ORIENT, 0.0001));
     }
 
     {
-        auto result = CGM_XYZ::localToLocal<L2LMAT3P_DIRECTION>(L2LMAT3P_A_COORD, L2LMAT3P_A_MAT3, L2LMAT3P_A_P, L2LMAT3P_B_QUAT);
-        ASSERT_TRUE(CGM::eq(result, L2LMAT3P_B_COORD_DIR_DIR, 0.0001));
+        auto result = CGM_XYZ::localToLocal<CGM_DIRECTION>(A_LOCAL_COORD, A_MAT3, A_P, B_QUAT);
+        ASSERT_TRUE(CGM::eq(result, A_LOCAL_COORD_CONVERTED_TO_LOCAL_B__ORIENT_2_ORIENT, 0.0001));
     }
 }
 
@@ -99,13 +80,15 @@ TEST(Cartesian_3D_Functions_Converters, LocalToLocal_Mat3WithPos_Quat)
 
 TEST(Cartesian_3D_Functions_Converters, LocalToLocal_Mat3WithPos_QuatWithPos)
 {
+    using namespace cgm_xyz_converters_tests_data;
+
     {
-        auto result = CGM_XYZ::localToLocal<L2LMAT3P_POINT>(L2LMAT3P_A_COORD, L2LMAT3P_A_MAT3, L2LMAT3P_A_P, L2LMAT3P_B_QUAT, L2LMAT3P_B_P);
-        ASSERT_TRUE(CGM::eq(result, L2LMAT3P_B_COORD_PT_PT, 0.0001));
+        auto result = CGM_XYZ::localToLocal<CGM_POINT>(A_LOCAL_COORD, A_MAT3, A_P, B_QUAT, B_P);
+        ASSERT_TRUE(CGM::eq(result, A_LOCAL_COORD_CONVERTED_TO_LOCAL_B__SPACE_2_SPACE, 0.0001));
     }
 
     {
-        auto result = CGM_XYZ::localToLocal<L2LMAT3P_DIRECTION>(L2LMAT3P_A_COORD, L2LMAT3P_A_MAT3, L2LMAT3P_A_P, L2LMAT3P_B_QUAT, L2LMAT3P_B_P);
-        ASSERT_TRUE(CGM::eq(result, L2LMAT3P_B_COORD_DIR_DIR, 0.0001));
+        auto result = CGM_XYZ::localToLocal<CGM_DIRECTION>(A_LOCAL_COORD, A_MAT3, A_P, B_QUAT, B_P);
+        ASSERT_TRUE(CGM::eq(result, A_LOCAL_COORD_CONVERTED_TO_LOCAL_B__ORIENT_2_ORIENT, 0.0001));
     }
 }
--- develop/tests/Cartesian/3D/Functions/Converters/LocalToLocal/QuaternionWithPosition.cpp
+++ develop/tests/Cartesian/3D/Functions/Converters/LocalToLocal/QuaternionWithPosition.cpp
@@ -2,48 +2,25 @@
 
 #include <iostream>
 #include <gtest/gtest.h>
-#include <CGM/Modules/Core/Operators/Matrix.hpp>
-#include <CGM/Modules/Core/Functions/Matrix.hpp>
 #include <CGM/Modules/Core/Functions/Vector.hpp>
-#include <CGM/Modules/Core/Functions/Quaternion.hpp>
-#include <CGM/Modules/Cartesian/3D/Types/Enums.hpp>
-#include <CGM/Modules/Cartesian/3D/Functions/Utils.hpp>
 #include <CGM/Modules/Cartesian/3D/Functions/Converters.hpp>
-#include <CGM/Modules/Cartesian/3D/Functions/Orientation.hpp>
+#include "Input.hpp"
 
 
 using namespace std;
-using namespace CGM;
-
-constexpr EVectorRepresentation L2LQUATP_POINT = EVectorRepresentation::Point;
-constexpr EVectorRepresentation L2LQUATP_DIRECTION = EVectorRepresentation::Direction;
-
-static const auto L2LQUATP_A_COORD = Vector<3,double>{ 3.13, 2.2, 1.7 };
-
-static const auto L2LQUATP_A_P = Vector<3,double>{ -1.200000, -0.600000, +4.40000 };
-static const auto L2LQUATP_A_QUAT = Quaternion<double>{ -0.006227, 0.435855, 0.174342, 0.882948 };
-
-static const auto L2LQUATP_B_X = Vector<3,double>{ +0.915829, -0.386754, -0.108065 };
-static const auto L2LQUATP_B_Y = Vector<3,double>{ +0.367711, +0.915829, -0.161386 };
-static const auto L2LQUATP_B_Z = Vector<3,double>{ +0.161386, +0.108065, +0.980957 };
-static const auto L2LQUATP_B_P = Vector<3,double>{ +4.030000, +1.700000, +2.200000 };
-static const auto L2LQUATP_B_QUAT = Quaternion<double>{ -0.068998, 0.068998, -0.193196, 0.976296 };
-static const auto L2LQUATP_B_MAT4 = CGM_XYZ::spaceMatrix(L2LQUATP_B_X, L2LQUATP_B_Y, L2LQUATP_B_Z, L2LQUATP_B_P);
-static const auto L2LQUATP_B_MAT3 = CGM_XYZ::orientationMatrix(L2LQUATP_B_X, L2LQUATP_B_Y, L2LQUATP_B_Z);
-static const auto L2LQUATP_B_COORD_PT_PT = Vector<3,double>{ -3.166704, +1.353024, +6.503487 };
-static const auto L2LQUATP_B_COORD_PT_DIR = Vector<3,double>{ +1.504246, +1.589060, +7.951732 };
-static const auto L2LQUATP_B_COORD_DIR_DIR = Vector<3,double>{ +2.113769, +1.198966, +3.409012 };
 
 TEST(Cartesian_3D_Functions_Converters, LocalToLocal_QuatWithPos_Mat3)
 {
+    using namespace cgm_xyz_converters_tests_data;
+
     {
-        auto result = CGM_XYZ::localToLocal<L2LQUATP_POINT>(L2LQUATP_A_COORD, L2LQUATP_A_QUAT, L2LQUATP_A_P, L2LQUATP_B_MAT3);
-        ASSERT_TRUE(CGM::eq(result, L2LQUATP_B_COORD_PT_DIR, 0.0001));
+        auto result = CGM_XYZ::localToLocal<CGM_POINT>(A_LOCAL_COORD, A_QUAT, A_P, B_MAT3);
+        ASSERT_TRUE(CGM::eq(result, A_LOCAL_COORD_CONVERTED_TO_LOCAL_B__SPACE_2_ORIENT, 0.0001));
     }
 
     {
-        auto result = CGM_XYZ::localToLocal<L2LQUATP_DIRECTION>(L2LQUATP_A_COORD, L2LQUATP_A_QUAT, L2LQUATP_A_P, L2LQUATP_B_MAT3);
-        ASSERT_TRUE(CGM::eq(result, L2LQUATP_B_COORD_DIR_DIR, 0.0001));
+        auto result = CGM_XYZ::localToLocal<CGM_DIRECTION>(A_LOCAL_COORD, A_QUAT, A_P, B_MAT3);
+        ASSERT_TRUE(CGM::eq(result, A_LOCAL_COORD_CONVERTED_TO_LOCAL_B__ORIENT_2_ORIENT, 0.0001));
     }
 }
 
@@ -51,14 +28,16 @@ TEST(Cartesian_3D_Functions_Converters, LocalToLocal_QuatWithPos_Mat3)
 
 TEST(Cartesian_3D_Functions_Converters, LocalToLocal_QuatWithPos_Mat3WithPos)
 {
+    using namespace cgm_xyz_converters_tests_data;
+
     {
-        auto result = CGM_XYZ::localToLocal<L2LQUATP_POINT>(L2LQUATP_A_COORD, L2LQUATP_A_QUAT, L2LQUATP_A_P, L2LQUATP_B_MAT3, L2LQUATP_B_P);
-        ASSERT_TRUE(CGM::eq(result, L2LQUATP_B_COORD_PT_PT, 0.0001));
+        auto result = CGM_XYZ::localToLocal<CGM_POINT>(A_LOCAL_COORD, A_QUAT, A_P, B_MAT3, B_P);
+        ASSERT_TRUE(CGM::eq(result, A_LOCAL_COORD_CONVERTED_TO_LOCAL_B__SPACE_2_SPACE, 0.0001));
     }
 
     {
-        auto result = CGM_XYZ::localToLocal<L2LQUATP_DIRECTION>(L2LQUATP_A_COORD, L2LQUATP_A_QUAT, L2LQUATP_A_P, L2LQUATP_B_MAT3, L2LQUATP_B_P);
-        ASSERT_TRUE(CGM::eq(result, L2LQUATP_B_COORD_DIR_DIR, 0.0001));
+        auto result = CGM_XYZ::localToLocal<CGM_DIRECTION>(A_LOCAL_COORD, A_QUAT, A_P, B_MAT3, B_P);
+        ASSERT_TRUE(CGM::eq(result, A_LOCAL_COORD_CONVERTED_TO_LOCAL_B__ORIENT_2_ORIENT, 0.0001));
     }
 }
 
@@ -66,14 +45,16 @@ TEST(Cartesian_3D_Functions_Converters, LocalToLocal_QuatWithPos_Mat3WithPos)
 
 TEST(Cartesian_3D_Functions_Converters, LocalToLocal_QuatWithPos_Mat4)
 {
+    using namespace cgm_xyz_converters_tests_data;
+
     {
-        auto result = CGM_XYZ::localToLocal<L2LQUATP_POINT>(L2LQUATP_A_COORD, L2LQUATP_A_QUAT, L2LQUATP_A_P, L2LQUATP_B_MAT4);
-        ASSERT_TRUE(CGM::eq(result, L2LQUATP_B_COORD_PT_PT, 0.0001));
+        auto result = CGM_XYZ::localToLocal<CGM_POINT>(A_LOCAL_COORD, A_QUAT, A_P, B_MAT4);
+        ASSERT_TRUE(CGM::eq(result, A_LOCAL_COORD_CONVERTED_TO_LOCAL_B__SPACE_2_SPACE, 0.0001));
     }
 
     {
-        auto result = CGM_XYZ::localToLocal<L2LQUATP_DIRECTION>(L2LQUATP_A_COORD, L2LQUATP_A_QUAT, L2LQUATP_A_P, L2LQUATP_B_MAT4);
-        ASSERT_TRUE(CGM::eq(result, L2LQUATP_B_COORD_DIR_DIR, 0.0001));
+        auto result = CGM_XYZ::localToLocal<CGM_DIRECTION>(A_LOCAL_COORD, A_QUAT, A_P, B_MAT4);
+        ASSERT_TRUE(CGM::eq(result, A_LOCAL_COORD_CONVERTED_TO_LOCAL_B__ORIENT_2_ORIENT, 0.0001));
     }
 }
 
@@ -81,14 +62,16 @@ TEST(Cartesian_3D_Functions_Converters, LocalToLocal_QuatWithPos_Mat4)
 
 TEST(Cartesian_3D_Functions_Converters, LocalToLocal_QuatWithPos_Quat)
 {
+    using namespace cgm_xyz_converters_tests_data;
+
     {
-        auto result = CGM_XYZ::localToLocal<L2LQUATP_POINT>(L2LQUATP_A_COORD, L2LQUATP_A_QUAT, L2LQUATP_A_P, L2LQUATP_B_QUAT);
-        ASSERT_TRUE(CGM::eq(result, L2LQUATP_B_COORD_PT_DIR, 0.0001));
+        auto result = CGM_XYZ::localToLocal<CGM_POINT>(A_LOCAL_COORD, A_QUAT, A_P, B_QUAT);
+        ASSERT_TRUE(CGM::eq(result, A_LOCAL_COORD_CONVERTED_TO_LOCAL_B__SPACE_2_ORIENT, 0.0001));
     }
 
     {
-        auto result = CGM_XYZ::localToLocal<L2LQUATP_DIRECTION>(L2LQUATP_A_COORD, L2LQUATP_A_QUAT, L2LQUATP_A_P, L2LQUATP_B_QUAT);
-        ASSERT_TRUE(CGM::eq(result, L2LQUATP_B_COORD_DIR_DIR, 0.0001));
+        auto result = CGM_XYZ::localToLocal<CGM_DIRECTION>(A_LOCAL_COORD, A_QUAT, A_P, B_QUAT);
+        ASSERT_TRUE(CGM::eq(result, A_LOCAL_COORD_CONVERTED_TO_LOCAL_B__ORIENT_2_ORIENT, 0.0001));
     }
 }
 
@@ -96,13 +79,15 @@ TEST(Cartesian_3D_Functions_Converters, LocalToLocal_QuatWithPos_Quat)
 
 TEST(Cartesian_3D_Functions_Converters, LocalToLocal_QuatWithPos_QuatWithPos)
 {
+    using namespace cgm_xyz_converters_tests_data;
+
     {
-        auto result = CGM_XYZ::localToLocal<L2LQUATP_POINT>(L2LQUATP_A_COORD, L2LQUATP_A_QUAT, L2LQUATP_A_P, L2LQUATP_B_QUAT, L2LQUATP_B_P);
-        ASSERT_TRUE(CGM::eq(result, L2LQUATP_B_COORD_PT_PT, 0.0001));
+        auto result = CGM_XYZ::localToLocal<CGM_POINT>(A_LOCAL_COORD, A_QUAT, A_P, B_QUAT, B_P);
+        ASSERT_TRUE(CGM::eq(result, A_LOCAL_COORD_CONVERTED_TO_LOCAL_B__SPACE_2_SPACE, 0.0001));
     }
 
     {
-        auto result = CGM_XYZ::localToLocal<L2LQUATP_DIRECTION>(L2LQUATP_A_COORD, L2LQUATP_A_QUAT, L2LQUATP_A_P, L2LQUATP_B_QUAT, L2LQUATP_B_P);
-        ASSERT_TRUE(CGM::eq(result, L2LQUATP_B_COORD_DIR_DIR, 0.0001));
+        auto result = CGM_XYZ::localToLocal<CGM_DIRECTION>(A_LOCAL_COORD, A_QUAT, A_P, B_QUAT, B_P);
+        ASSERT_TRUE(CGM::eq(result, A_LOCAL_COORD_CONVERTED_TO_LOCAL_B__ORIENT_2_ORIENT, 0.0001));
     }
 }
