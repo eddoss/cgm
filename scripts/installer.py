e16a2ac03c1562fa1b4b54cb4d6c00342243cf53
Edward Sarkisyan <edw.sarkisyan@gmail.com>
2020-10-10 02:26:50 +0200

    [scripts]: update library installer script.

--- scripts/installer.py
+++ scripts/installer.py
@@ -1,37 +1,36 @@
 import os
-import shutil
+
+def generate(properties, source_dir, build_dir):
+    cmd = f'cmake -S {source_dir} -B {build_dir} -G "Visual Studio 15 2017" -A x64'
+    for prop in properties:
+        cmd += f" -D{prop}={props[prop]}"
+    os.system(cmd)
+
+def build(build_dir, config, target):
+    cmd = f'cmake --build {build_dir} --target {target} --config {config}'
+    os.system(cmd)
+
+def install(build_dir, config):
+    cmd = f'cmake --build {build_dir} --target INSTALL --config {config}'
+    os.system(cmd)
 
 
-def clear(dir: str):
-    shutil.rmtree(dir, True)
 
-def build(labels, mat_mult, handedness, matrix_storing, double_precis):
-    root = os.path.abspath(os.path.join(os.getcwd(), '../'))
-    source_dir = root
-    install_dir = os.path.join(root, 'install')
-    build_dir = os.path.join(install_dir, 'build')
+dir_root    = os.path.abspath(os.path.join(os.getcwd(), '../'))
+dir_source  = dir_root
+dir_install = os.path.join(dir_root, 'install')
+dir_build   = os.path.join(dir_install, 'build')
 
-    props = {
-        "CMAKE_INSTALL_PREFIX": install_dir,
-        "CGM_BUILD_UNIT_TESTS": "OFF",
-        "CGM_BUILD_EXAMPLES": "OFF",
-        "CGM_USE_DOUBLE_PRECISION": double_precis,
-        "CGM_MATRIX_LAYOUT": matrix_storing,
-        "CGM_HANDEDNESS": handedness,
-        "CGM_RIGHT_UP_FORWARD_AXES_LABELS": labels,
-        "CGM_MATRIX_MULTIPLICATION_ORDER": mat_mult
-    }
+props = {
+    "CMAKE_INSTALL_PREFIX": dir_install,
+    "CGM_BUILD_UNIT_TESTS": "OFF",
+    "CGM_BUILD_EXAMPLES": "OFF",
+    "CGM_USE_DOUBLE_PRECISION": "OFF",
+    "CGM_MATRIX_LAYOUT": "Column",
+    "CGM_HANDEDNESS": "Right",
+    "CGM_RIGHT_UP_FORWARD_AXES_LABELS": "XYZ",
+    "CGM_MATRIX_MULTIPLICATION_ORDER": "Post"
+}
 
-    cmd_gen = f'cmake -S {source_dir} -B {build_dir} -G "Visual Studio 15 2017" -A x64'
-    for prop in props:
-        cmd_gen += f" -D{prop}={props[prop]}"
-
-    cmd_install = f'cmake --build {build_dir} --target INSTALL --config Release'
-
-    # clear(build_dir)
-    # clear(install_dir)
-
-    os.system(cmd_gen)
-    os.system(cmd_install)
-
-build('XYZ', 'Post', 'Right', 'Column', 'OFF')
+generate(props, dir_source, dir_build)
+install(dir_build, "Release")
