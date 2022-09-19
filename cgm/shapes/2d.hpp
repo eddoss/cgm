611b5217b80625522f81c20e3db70778c885f778
Edward Sarkisyan <edw.sarkisyan@gmail.com>
2022-09-19 10:32:58 +0200

    [shapes::2D]: expose 'shapes/2d'

---
+++ cgm/shapes/2d.hpp
@@ -0,0 +1,12 @@
+#include <cgm/modules/shapes/module_globals.hpp>
+
+#include <cgm/modules/shapes/2d/module_globals.hpp>
+#include <cgm/modules/shapes/2d/types/rectangle.hpp
+#include <cgm/modules/shapes/2d/types/circle.hpp>
+#include <cgm/modules/shapes/2d/types/range.hpp>
+#include <cgm/modules/shapes/2d/types/typedefs.hpp>
+
+#include <cgm/modules/shapes/2d/functions/size.hpp>
+#include <cgm/modules/shapes/2d/functions/corners.hpp>
+#include <cgm/modules/shapes/2d/functions/contains.hpp>
+#include <cgm/modules/shapes/2d/functions/overlap.hpp>
